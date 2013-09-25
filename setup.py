54085ae6c007465d5c8b580806781c0e83c12566
John Lynn <jlynn@hearsaycorp.com>
2013-09-25 09:30:15 -0700

    Update version info for Sentry 6.0 support

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import find_packages
 from setuptools import setup
 
 install_requires = [
-    'sentry>=5.0.0',
+    'sentry>=6.0.0',
 ]
 
 setup(
     name='sentry-subscriptions',
-    version='0.1.0',
+    version='0.2.0',
     author='John Lynn',
     author_email='jlynn@hearsaycorp.com',
     package_dir={'': 'src'},
