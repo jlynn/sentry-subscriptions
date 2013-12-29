90a4ff94dcc28940e062f454695438d08430ece9
Roger Hu <rhu@hearsaycorp.com>
2013-12-29 01:42:52 +0000

    UnicodeSafePynLiner has been moved to sentry.utils.email.
    Sentry v6.4.3 has a new MessageBuilder we should also consider using.

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import find_packages
 from setuptools import setup
 
 install_requires = [
-    'sentry>=6.0.0',
+    'sentry>=6.4.3',
 ]
 
 setup(
     name='sentry-subscriptions',
-    version='0.2.0',
+    version='0.2.1',
     author='John Lynn',
     author_email='jlynn@hearsaycorp.com',
     package_dir={'': 'src'},
